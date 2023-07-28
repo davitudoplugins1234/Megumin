fd43b3a81eaf98245c4ad8443927e2e1026e543c
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-07-28 18:13:38 -0300

    Update cotação.py

--- megumin/modulos/cotação.py
+++ megumin/modulos/cotação.py
@@ -4,37 +4,37 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import Message
 
-from megumin import megux
+from megumin import megux, Config
 
-@megux.on_message(filters.command(["cota"], prefixes=["/", "!"]))
+@megux.on_message(filters.command(["cota"], Config.TRIGGER))
 async def pegar_cotacoes(_, message):
-    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,JPY-BRL,BTC-BRL,ETH-BRL,XRP-BRL,DOGE-BRL,ARS-BRL,RUB-BRL")
+    req = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,JPY-BRL,BTC-BRL,ETH-BRL,XRP-BRL,DOGE-BRL,ARS-BRL,RUB-BRL")
 
-    requisicao_dic = requisicao.json()
+    res = req.json()
 
-    cotacao_dolar = requisicao_dic['USDBRL']['bid']
-    dat_dolar = requisicao_dic ['USDBRL']['create_date']
-    var_dolar = requisicao_dic ['USDBRL']['varBid']
-    cotacao_euro = requisicao_dic['EURBRL']['bid']
-    dat_euro = requisicao_dic['EURBRL']['create_date']
-    var_euro = requisicao_dic ['EURBRL']['varBid']
-    cotacao_btc = requisicao_dic['BTCBRL']['bid']
-    dat_btc = requisicao_dic['BTCBRL']['create_date']
-    var_btc = requisicao_dic ['BTCBRL']['varBid']
-    cotacao_iene = requisicao_dic ['JPYBRL']['bid']
-    dat_iene = requisicao_dic ['JPYBRL']['create_date']
-    var_iene = requisicao_dic ['JPYBRL']['varBid']
-    cotacao_doge = requisicao_dic ['DOGEBRL']['bid']
-    dat_doge = requisicao_dic ['DOGEBRL']['create_date']
-    var_doge = requisicao_dic ['DOGEBRL']['varBid']
-    cotacao_ars = requisicao_dic ['ARSBRL']['bid']
-    dat_ars = requisicao_dic ['ARSBRL']['create_date']
-    var_ars = requisicao_dic ['ARSBRL']['varBid']
-    cotacao_rub = requisicao_dic ['RUBBRL']['bid']
-    dat_rub = requisicao_dic ['RUBBRL']['create_date']
-    var_rub = requisicao_dic ['RUBBRL']['varBid']
+    cotacao_dolar = res['USDBRL']['bid']
+    dat_dolar = res['USDBRL']['create_date']
+    var_dolar = res['USDBRL']['varBid']
+    cotacao_euro = res['EURBRL']['bid']
+    dat_euro = res['EURBRL']['create_date']
+    var_euro = res['EURBRL']['varBid']
+    cotacao_btc = res['BTCBRL']['bid']
+    dat_btc = res['BTCBRL']['create_date']
+    var_btc = res['BTCBRL']['varBid']
+    cotacao_iene = res['JPYBRL']['bid']
+    dat_iene = res['JPYBRL']['create_date']
+    var_iene = res['JPYBRL']['varBid']
+    cotacao_doge = res['DOGEBRL']['bid']
+    dat_doge = res['DOGEBRL']['create_date']
+    var_doge = res['DOGEBRL']['varBid']
+    cotacao_ars = res['ARSBRL']['bid']
+    dat_ars = res['ARSBRL']['create_date']
+    var_ars = res['ARSBRL']['varBid']
+    cotacao_rub = res['RUBBRL']['bid']
+    dat_rub = res['RUBBRL']['create_date']
+    var_rub = res['RUBBRL']['varBid']
 
-    obting_info = await message.reply(f"""```Obtendo informações sobre as moedas...```""")
+    obting_info = await message.reply(f"<i>Obtendo informações sobre as moedas...</i>")
     await asyncio.sleep(0.3)
     await obting_info.delete()
 
