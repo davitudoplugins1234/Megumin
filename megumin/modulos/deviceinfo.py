f9a5b4b11a769d27812c6f78f2574d9594e48d86
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-08-15 19:22:47 -0300

    Update deviceinfo.py

--- megumin/modulos/deviceinfo.py
+++ megumin/modulos/deviceinfo.py
@@ -17,27 +17,27 @@ async def deviceinfo(c: megux, m: Message):
         name = input_str(m).lower() 
         searchi = f"{name}".replace(" ", "+")
         get_search_api = await search_device(searchi)
-        if not get_search_api == []:
+        if get_search_api:
             # Access the link from the first search result  
-            name = get_search_api[0]["name"]
-            img = get_search_api[0]["img"]
-            id = get_search_api[0]["id"]
+            name = get_search_api[0].get("name", "N/A")
+            img = get_search_api[0].get("img", "N/A")
+            id = get_search_api[0].get("id", "N/A")
             link = f"https://www.gsmarena.com/{id}.php"
-            description = get_search_api[0]["description"]
+            description = get_search_api[0].get("description", "N/A")
             try:
                 get_device_api = await get_device(id)
-                name_cll = get_device_api["name"]
-                s1 = get_device_api['detailSpec'][0]['specifications'][0]['value']
-                s1_name = get_device_api['detailSpec'][0]['specifications'][0]['name']
-                s2 = get_device_api['detailSpec'][1]['specifications'][0]['value']
-                s2_name = get_device_api['detailSpec'][1]['specifications'][0]['name']
-                s3 = get_device_api['detailSpec'][4]['specifications'][1]['value']
-                s3_name = get_device_api['detailSpec'][4]['specifications'][1]['name']
-                s4 = get_device_api['detailSpec'][3]['specifications'][1]['value']
-                s4_name = get_device_api['detailSpec'][3]['specifications'][1]['name']
-                s5 = get_device_api['detailSpec'][2]['specifications'][3]['value']
-                s5_name = get_device_api['detailSpec'][2]['specifications'][3]['name']
-                await m.reply(f"<b>Foto Device</b>: {img}\n<b>URL Fonte</b>: https://www.gsmarena.com/{id}.php\n\n<b>- Aparelho</b>:  <i>{name_cll}</i>\n<b>- {s1_name}</b>: <i>{s1}</i>\n<b>- {s2_name}</b>: <i>{s2}</i>\n<b>- {s3_name}</b>: <i>{s3}</i>\n<b>- {s4_name}</b>: <i>{s4}</i>\n<b>- {s5_name}</b>: <i>{s5}</i>\n\n<b>Descrição</b>: {description}", disable_web_page_preview=False)
+                name_cll = get_device_api.get("name", "N/A")
+                s1 = get_device_api['detailSpec'][0]['specifications'][0].get('value', 'N/A')
+                s1_name = get_device_api['detailSpec'][0]['specifications'][0].get('name', 'N/A')
+                s2 = get_device_api['detailSpec'][1]['specifications'][0].get('value', 'N/A')
+                s2_name = get_device_api['detailSpec'][1]['specifications'][0].get('name', 'N/A')
+                s3 = get_device_api['detailSpec'][4]['specifications'][1].get('value', 'N/A')
+                s3_name = get_device_api['detailSpec'][4]['specifications'][1].get('name', 'N/A')
+                s4 = get_device_api['detailSpec'][3]['specifications'][1].get('value', 'N/A')
+                s4_name = get_device_api['detailSpec'][3]['specifications'][1].get('name', 'N/A')
+                s5 = get_device_api['detailSpec'][2]['specifications'][3].get('value', 'N/A')
+                s5_name = get_device_api['detailSpec'][2]['specifications'][3].get('name', 'N/A')
+                await m.reply(f"<b>Photo Device</b>: {img}\n<b>URL Font</b>: https://www.gsmarena.com/{id}.php\n\n<b>- Device</b>:  <i>{name_cll}</i>\n<b>- {s1_name}</b>: <i>{s1}</i>\n<b>- {s2_name}</b>: <i>{s2}</i>\n<b>- {s3_name}</b>: <i>{s3}</i>\n<b>- {s4_name}</b>: <i>{s4}</i>\n<b>- {s5_name}</b>: <i>{s5}</i>\n\n<b>Description</b>: {description}", disable_web_page_preview=False)
             except Exception as err:
                 return await m.reply(f"Não consegui obter resultados sobre o aparelho. O gsmarena pode estar offline. <i>Erro</i>: <b>{err}</b>")
         else:
