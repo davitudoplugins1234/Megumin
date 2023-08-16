12cb46f43ccc887229c8b5a6dce1f6a09c5f6aee
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-08-16 17:26:34 -0300

    Test

--- megumin/modulos/medias.py
+++ megumin/modulos/medias.py
@@ -69,6 +69,8 @@ async def ytdlcmd(c: megux, m: Message):
     else:
         yt = await extract_info(ydl, rege.group(), download=False)
 
+    print(yt["formats"])
+    
     for f in yt["formats"]:
         if f["format_id"] == "140":
             afsize = f["filesize"] or 0
