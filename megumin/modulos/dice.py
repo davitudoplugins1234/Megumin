e80efd1b10bb7bf0da3e854138296305f0278d3a
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-08-17 22:18:16 -0300

    Update dice.py

--- megumin/modulos/dice.py
+++ megumin/modulos/dice.py
@@ -6,6 +6,6 @@ from megumin import megux
 
 @megux.on_message(filters.command(["dice", "dado"], prefixes=["/", "!"]))
 async def dice(c: megux, m: Message):
-    dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.message_id)
+    dicen = await c.send_dice(m.chat.id, reply_to_message_id=m.id)
     await dicen.reply_text("🎲 O dado parou no número: {number}".format(number=dicen.dice.value), quote=True
     )
