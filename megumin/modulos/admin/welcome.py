fb683652331e8dd4b589e6ac655a44a16753775d
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-08-18 18:00:36 -0300

    Update welcome.py

--- megumin/modulos/admin/welcome.py
+++ megumin/modulos/admin/welcome.py
@@ -193,8 +193,8 @@ async def greet_new_members(c: megux, m: Message):
                 count=count,
             )
 
-            if not await find_user(user_id):
-                await add_user(user_id)
+            if not await find_user(m.from_user.id):
+                await add_user(m.from_user.id)
             
             welcome, welcome_buttons = button_parser(welcome)
             if await captcha.find_one({"chat_id": m.chat.id, "status": True}):
