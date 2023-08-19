9a56c00d40af14a7ac0485ce1979a4796f5aaa2a
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-08-19 18:41:09 -0300

    Update logger.py

--- megumin/utils/logger.py
+++ megumin/utils/logger.py
@@ -2,10 +2,19 @@ __all__ = ["logging"]
 
 import logging
 
+from logging import RotatingFileHandler
+
 # Logs de console
 
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s - %(levelname)s] - %(name)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    handlers=[
+                        RotatingFileHandler(
+                            "WhiterKang-Logs.log", maxBytes=20480, backupCount=10),
+                        logging.StreamHandler()
+                    ])
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.parser.html").setLevel(logging.ERROR)
+logging.getLogger("pyrogram.session.session").setLevel(logging.ERROR)
