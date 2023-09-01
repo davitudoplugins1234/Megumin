4e1796a899e45720144373bd39620091bf0dbd51
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-09-01 13:26:32 -0300

    Update tools.py

--- megumin/utils/tools.py
+++ megumin/utils/tools.py
@@ -171,9 +171,10 @@ async def extract_time(msg, time_val):
 async def cssworker_url(target_url: str, pc_id: str):
     url = "https://htmlcsstoimage.com/demo_run"
     my_headers = {
-        "User-Agent": f"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0 [PC-ID({pc_id})]", 
-        "Accept-Language': "pt-BR",
-    }
+    "User-Agent": f"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0 [PC-ID({pc_id})]",
+    "Accept-Language": "pt-BR"
+}
+
 
     data = {
         "url": target_url,
