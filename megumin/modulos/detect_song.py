2abfd90734bc39501ce98afd76877e9433c3742d
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-09-11 19:42:23 -0300

    Update detect_song.py

--- megumin/modulos/detect_song.py
+++ megumin/modulos/detect_song.py
@@ -1,10 +1,5 @@
 import os
-import spacy
-import uuid
 import asyncio
-import speech_recognition as sr
-import subprocess
-
 
 from shazamio import Shazam
 
@@ -55,44 +50,3 @@ async def which_song(c: megux, message: Message):
         os.remove(file)
         return await sent.edit("<i>Failed to get sound data.</i>")
     os.remove(file)
-
-    
-@megux.on_message(filters.voice)
-@disableable_dec("speech")
-async def transcriber(c: megux, m: Message):
-    if not await find_user(m.from_user.id):
-        await add_user(m.from_user.id)
-    if m.voice:
-        sent = await m.reply("<i>Fazendo Download do Áudio..</i>")
-        try:
-            file = await c.download_media(
-                        message=m.voice,
-                        file_name=Config.DOWN_PATH
-                    )
-        except Exception as e:
-            await sent.edit(f"<i>Ocorreu um erro: {e}")
-         
-        try:
-            str_uuid = str(uuid.uuid4())
-            wav_file_path = f"{Config.DOWN_PATH}Transcriber{str_uuid}_WAV.wav"
-            ffmpeg_cmd = f"ffmpeg -i {file} {wav_file_path}"
-            subprocess.run(ffmpeg_cmd, shell=True)
-        except Exception as e:
-            await sent.edit(f"Ocorreu um erro: {e}")
-        
-        with sr.AudioFile(wav_file_path) as source:
-            audio = recognizer.record(source)
-            try:
-                await sent.edit("Transcrevendo Fala em Texto...")
-                text = recognizer.recognize_google(audio, language="pt-BR")
-                await sent.edit(f"<b>Texto:</b>\n<i>{text}</i>")
-            except sr.UnknownValueError:
-                await sent.edit("<i>Não consegui, Identificar o que você disse.")
-                await asyncio.sleep(5)
-                await sent.delete()
-            except sr.RequestError:
-                await sent.edit("<i>O Serviço de conversão de fala em texto, Não está disponivel no momento.</i>")
-                await asyncio.sleep(5)
-                await sent.delete()
-    os.remove(file)
-    os.remove(wav_file_path)
